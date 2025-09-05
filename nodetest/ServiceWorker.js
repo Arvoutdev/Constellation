512f13eeb8921aff6e5303d82a726b832816b88d
Arvoutdev <arvoutdev@gmail.com>
2025-09-05 18:37:16 -0400

    3.1

---
+++ nodetest/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "agent0-Fusion Zero-z0.268";
+const contentToCache = [
+    "Build/nodetest.loader.js",
+    "Build/nodetest.framework.js.unityweb",
+    "Build/nodetest.data.unityweb",
+    "Build/nodetest.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
