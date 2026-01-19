3e7f73b3305cfa62ce4ae05b5cde226bb8b1fcc8
Arvoutdev <arvoutdev@gmail.com>
2026-01-18 21:18:41 -0500

    ay new fusion version

--- nodetest/ServiceWorker.js
+++ nodetest/ServiceWorker.js
@@ -1,33 +1,42 @@
-const cacheName = "agent0-Fusion Zero-z0.35";
+const cacheName = "agent0-Fusion Zero-z0.36";
 const contentToCache = [
     "Build/nodetest.loader.js",
     "Build/nodetest.framework.js.unityweb",
     "Build/nodetest.data.unityweb",
     "Build/nodetest.wasm.unityweb",
     "TemplateData/style.css"
-
 ];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
-    
+    self.skipWaiting();
     e.waitUntil((async function () {
       const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
       await cache.addAll(contentToCache);
     })());
 });
 
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== cacheName) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
 self.addEventListener('fetch', function (e) {
     e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      const response = await caches.match(e.request);
       if (response) { return response; }
 
-      response = await fetch(e.request);
+      const networkResponse = await fetch(e.request);
       const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
+      cache.put(e.request, networkResponse.clone());
+      return networkResponse;
     })());
 });
